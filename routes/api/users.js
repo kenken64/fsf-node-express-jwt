040d07cdf8f413977e207ca0ee6d6fa37b8b06bd
Kenneth Phang <bunnyppl@gmail.com>
2018-04-06 08:56:49 +0800

    first version

---
+++ routes/api/users.js
@@ -0,0 +1,78 @@
+var router = require('express').Router();
+var passport = require('passport');
+
+var auth = require('../auth');
+var jwt = require('jsonwebtoken');
+var secret = require('../../config').secret;
+
+router.get('/user', auth.required, function(req, res, next){
+  return res.json({});
+});
+
+router.put('/user', auth.required, function(req, res, next){
+  
+    // only update fields that were actually passed...
+    if(typeof req.body.user.username !== 'undefined'){
+      user.username = req.body.user.username;
+    }
+    if(typeof req.body.user.email !== 'undefined'){
+      user.email = req.body.user.email;
+    }
+    if(typeof req.body.user.bio !== 'undefined'){
+      user.bio = req.body.user.bio;
+    }
+    if(typeof req.body.user.image !== 'undefined'){
+      user.image = req.body.user.image;
+    }
+    if(typeof req.body.user.password !== 'undefined'){
+      user.setPassword(req.body.user.password);
+    }
+
+    return res.json({});
+});
+
+router.post('/users/login', function(req, res, next){
+  if(!req.body.user.email){
+    return res.status(422).json({errors: {email: "can't be blank"}});
+  }
+
+  if(!req.body.user.password){
+    return res.status(422).json({errors: {password: "can't be blank"}});
+  }
+
+  passport.authenticate('local', {session: false}, function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      user.token = generateJWT();
+      return res.json({user: user});
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
+});
+
+router.post('/users', function(req, res, next){
+  
+  var username = req.body.user.username;
+  var email = req.body.user.email;
+  //user.setPassword(req.body.user.password);
+
+  return res.json({});
+});
+
+function generateJWT() {
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, secret);
+};
+
+
+
+module.exports = router;
